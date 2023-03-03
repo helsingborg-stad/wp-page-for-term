009e757dcddf4fb31e1a29403e56b5666e8e0b5d
Anna Johansson <anna@thehappyhipster.se>
2023-03-03 12:29:14 +0100

    Handle bi-directional relationship between page and term whenever on or the other is updated

--- source/php/App.php
+++ source/php/App.php
@@ -6,39 +6,52 @@ class App
 {
     public function __construct()
     {
-        add_action('admin_enqueue_scripts', array($this, 'enqueueStyles'));
-        add_action('admin_enqueue_scripts', array($this, 'enqueueScripts'));
 
-        $this->cacheBust = new \wpPageForTerm\Helper\CacheBust();
+        add_filter(
+            'acf/update_value/key=field_6401a495f45b5',
+            [$this, 'storeRelationshipTermToPage'],
+            10,
+            3
+        );
+
+        add_filter(
+            'acf/update_value/key=field_63fe0756de689',
+            [$this, 'storeRelationshipPageToTerm'],
+            10,
+            3
+        );
     }
 
     /**
-     * Enqueue required style
-     * @return void
-     */
-    public function enqueueStyles()
+     * Updates the relationship on the post side whenever a term is updated with a post relationship.
+     * It is called when the 'acf/update_value/key=field_6401a495f45b5' filter is applied.
+     *
+     * @param int $postId The ID of the post being added to the term.
+     * @param int $termId The ID of the term being updated.
+     *
+     * @return int The ID of the post being added to a term.
+     *
+    */
+    public function storeRelationshipTermToPage($postId, $termId)
     {
-        wp_register_style(
-            'wp-page-for-term-css',
-            WP_PLUGIN_PAGE_FOR_TERM_URL . '/dist/' .
-            $this->cacheBust->name('css/wp-page-for-term.css')
-        );
-
-        wp_enqueue_style('wp-page-for-term-css');
+        update_post_meta($postId, 'is_page_for_term', $termId);
+        return $postId;
     }
 
-    /**
-     * Enqueue required scripts
-     * @return void
-     */
-    public function enqueueScripts()
-    {
-        wp_register_script(
-            'wp-page-for-term-js',
-            WP_PLUGIN_PAGE_FOR_TERM_URL . '/dist/' .
-            $this->cacheBust->name('js/wp-page-for-term.js')
-        );
 
-        wp_enqueue_script('wp-page-for-term-js');
+    /**
+     * Updates the relationship on the term side whenever a post is updated with a term relationship.
+     * It is called when the 'acf/update_value/key=field_63fe0756de689' filter is applied.
+     *
+     * @param int $termId The ID of the term being added to a post.
+     * @param int $postId The ID of the post being updated.
+     *
+     * @return int The ID of the term being added to a post.
+     *
+    */
+    public function storeRelationshipPageToTerm($termId, $postId)
+    {
+        update_term_meta($termId, 'page_for_term', $postId);
+        return $termId;
     }
 }
