8a302b7afa25146b9e1c682f4541694fdbd36e8d
Anna Johansson <anna@thehappyhipster.se>
2023-03-03 15:30:49 +0100

    Handle bi-directional relationship between page and term whenever on or the other is updated

--- source/php/App.php
+++ source/php/App.php
@@ -4,54 +4,53 @@ namespace wpPageForTerm;
 
 class App
 {
+    protected const PAGE_FOR_TERM_FIELD_KEY = 'page_for_term';
+    protected const IS_PAGE_FOR_TERM_FIELD_KEY = 'is_page_for_term';
+
     public function __construct()
     {
-
-        add_filter(
-            'acf/update_value/key=field_6401a495f45b5',
-            [$this, 'storeRelationshipTermToPage'],
-            10,
-            3
-        );
-
-        add_filter(
-            'acf/update_value/key=field_63fe0756de689',
-            [$this, 'storeRelationshipPageToTerm'],
-            10,
-            3
-        );
+        add_action('acf/save_post', [$this, 'updatePageForTerm'], 20);
+        add_action('acf/save_post', [$this, 'updateIsPageForTerm'], 20);
     }
 
     /**
-     * Updates the relationship on the post side whenever a term is updated with a post relationship.
-     * It is called when the 'acf/update_value/key=field_6401a495f45b5' filter is applied.
+     * Updates the "page_for_term" ACF field for each term ID that was updated on the post.
      *
-     * @param int $postId The ID of the post being added to the term.
-     * @param int $termId The ID of the term being updated.
-     *
-     * @return int The ID of the post being added to a term.
-     *
-    */
-    public function storeRelationshipTermToPage($postId, $termId)
+     * @param int $postId The ID of the post being saved.
+     */
+    public function updatePageForTerm($postId)
     {
-        update_post_meta($postId, 'is_page_for_term', $termId);
-        return $postId;
+        if (
+            !isset($_POST['acf'][self::PAGE_FOR_TERM_FIELD_KEY])
+            || !is_array($_POST['acf'][self::PAGE_FOR_TERM_FIELD_KEY])
+        ) {
+            return;
+        }
+
+        $termIds = $_POST['acf'][self::PAGE_FOR_TERM_FIELD_KEY];
+        foreach ($termIds as $termId) {
+            $termKey = "term_{$termId}";
+            update_field(self::PAGE_FOR_TERM_FIELD_KEY, $postId, $termKey);
+        }
     }
 
-
     /**
-     * Updates the relationship on the term side whenever a post is updated with a term relationship.
-     * It is called when the 'acf/update_value/key=field_63fe0756de689' filter is applied.
+     * Updates the "is_page_for_term" ACF field for the page associated with the current term ID.
      *
-     * @param int $termId The ID of the term being added to a post.
-     * @param int $postId The ID of the post being updated.
-     *
-     * @return int The ID of the term being added to a post.
-     *
-    */
-    public function storeRelationshipPageToTerm($termId, $postId)
+     * @param int $postId The ID of the post being saved.
+     */
+    public function updateIsPageForTerm($postId)
     {
-        update_term_meta($termId, 'page_for_term', $postId);
-        return $termId;
+        if (!isset($_POST['acf'][self::IS_PAGE_FOR_TERM_FIELD_KEY])) {
+            return;
+        }
+
+        $termId = (int) explode('_', $postId)[1];
+        $pageId = (int) $_POST['acf'][self::IS_PAGE_FOR_TERM_FIELD_KEY];
+
+        $terms = (array) get_field(self::PAGE_FOR_TERM_FIELD_KEY, $pageId);
+        $terms[] = $termId;
+
+        update_field(self::IS_PAGE_FOR_TERM_FIELD_KEY, $terms, $pageId);
     }
 }
