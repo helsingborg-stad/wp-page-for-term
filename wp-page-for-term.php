3f5cffa2e5e9163285caf3b54ef9595e23e38fcb
Sebastian Thulin <sebastian.thulin@helsingborg.se>
2023-08-31 14:33:54 +0200

    Autoload & Build

--- wp-page-for-term.php
+++ wp-page-for-term.php
@@ -20,12 +20,11 @@ if (! defined('WPINC')) {
 
 define('WP_PLUGIN_PAGE_FOR_TERM_PATH', plugin_dir_path(__FILE__));
 
-// Register the autoloaders
-require_once __DIR__ . '/vendor/autoload.php';
-
-foreach (glob(plugin_dir_path(__FILE__) . 'source/php/*.php') as $file) {
-    require_once $file;
+// Autoload from plugin
+if (file_exists(WP_PLUGIN_PAGE_FOR_TERM_PATH . 'vendor/autoload.php')) {
+    require_once WP_PLUGIN_PAGE_FOR_TERM_PATH . 'vendor/autoload.php';
 }
+
 // Acf auto import and export
 add_action('acf/init', function () {
     load_plugin_textdomain('wp-page-for-term', false, dirname(plugin_basename(__FILE__)) . '/languages');
